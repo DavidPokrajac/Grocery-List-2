2c4728e3196ead4c30b864fddfa180f4b42ec308
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-15 14:55:03 +0200

    Initial commit

---
+++ sandbox.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+
+  // Referencing HTML tags
+  var form = document.forms[0];
+  var input = form.querySelector("input[type='text']");
+  var addContainer = document.getElementById("add-cont");
+  var groceriesContainer = document.getElementById("groceries-cont");
+  var button = document.querySelector("button");
+
+  // Creating new element
+  var ul = document.createElement("ul");
+
+
+    
+
+}
